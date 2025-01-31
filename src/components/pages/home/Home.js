e4fb41055b797530734eae7493b6f15cbf03fded
Yassinedevs <122100038+Yassinedevs@users.noreply.github.com>
2025-01-31 15:27:50 +0100

    refactor: Redesign home page layout with split-screen and responsive styling

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,26 +1,41 @@
-import './Home.css';
+import React from "react";
+import "./Home.css";
 import BlueButton from "../../blue-button/BlueButton";
 
-function Home(){
-    return (
-        <section className="home"
-                 style={{
-                     backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/img/home_bg.png'})`,
-                     backgroundPosition: 'center',
-                     backgroundRepeat: 'no-repeat',
-                     backgroundSize: 'cover'
-                 }}>
-            <p className="home__text">Second step for second life</p>
-            <p className="home__title">Des vêtements d'occasion qui n'ont rien à envier au neuf.</p>
-            <section className="home__description">
-                <p className="home__text">RIFRAICH redéfinit la seconde main. Découvre des vêtements d'occasion triés
-                    sur le volet, pour une qualité premium et une expérience d'achat comme neuve.</p>
-                <p className="home__text">Pièces uniques, marques premium, garanties impeccables.</p>
-                <p className="home__text">Shoppe la mode durable sans compromis sur le style.</p>
-            </section>
-            <BlueButton link="https://rifraich.softr.app/newsletter" label="S'inscrire à la newsletter"/>
-        </section>
-    );
-}
+const Home = () => {
+  return (
+    <section className="home">
+      {/* Bloc gauche */}
+      <div className="home__left">
+        <div className="home__title-container">
+          <h1 className="home__title">La mode seconde main</h1>
+        </div>
+        <p className="home__description">
+          Simplifiez la gestion de votre boutique en ligne et concentrez-vous
+          sur ce que vous aimez : <span className="highlight">la mode</span>.
+        </p>
+        <BlueButton label="Commencer maintenant" link="/start" />
+      </div>
+
+      {/* Image au centre */}
+      <div className="home__image-container">
+        <img
+          src={process.env.PUBLIC_URL + "/assets/img/home_pro.png"}
+          alt="Mode Seconde Main"
+          className="home__image"
+        />
+      </div>
+
+      {/* Bloc droit */}
+      <div className="home__right">
+        <div className="home__title-container">
+          <h1 className="home__title home__title--blue">
+            prend un <br /> nouveau souffle
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Home;
