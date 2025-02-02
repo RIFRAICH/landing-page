07115d6ce783331c39d7080c1c60a96015905fd1
Yassinedevs <122100038+Yassinedevs@users.noreply.github.com>
2025-02-02 19:33:39 +0100

    feat: Create About page component

---
+++ src/components/pages/about/About.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { UilChartGrowth, UilBox, UilTruck } from '@iconscout/react-unicons';
+import './About.css';
+
+const About = () => {
+    return (
+        <section className="about">
+            <h2 className="about__title">À PROPOS DE NOUS</h2>
+            <h3 className="about__subtitle">
+                Rifraich vous offre une solution clé en main<br />
+                pour développer votre activité
+            </h3>
+            <div className="about__cards">
+                <div className="about__card">
+                    <div className="about__card-icon">
+                        <UilChartGrowth size="32" />
+                    </div>
+                    <h4 className="about__card-title">
+                        Augmentez la visibilité de votre boutique
+                    </h4>
+                    <p className="about__card-description">
+                        Bénéficiez d'une vitrine en ligne qui vous permettra d'atteindre une clientèle plus large et de développer votre notoriété
+                    </p>
+                </div>
+                <div className="about__card">
+                    <div className="about__card-icon">
+                        <UilBox size="32" />
+                    </div>
+                    <h4 className="about__card-title">
+                        Gérez facilement votre stock
+                    </h4>
+                    <p className="about__card-description">
+                        Simplifiez votre gestion de stock avec notre interface intuitive et suivez en temps réel vos ventes et vos stocks
+                    </p>
+                </div>
+                <div className="about__card">
+                    <div className="about__card-icon">
+                        <UilTruck size="32" />
+                    </div>
+                    <h4 className="about__card-title">
+                        Nous nous occupons de la livraison et du paiement
+                    </h4>
+                    <p className="about__card-description">
+                        Concentrez-vous sur votre activité. Nous prenons en charge la livraison et le paiement de vos commandes pour vous offrir une tranquillité d'esprit totale
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default About;
