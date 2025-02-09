4e8eff33bca2329ea83467f1a8a7a65bc1a77f72
Yassinedevs <122100038+Yassinedevs@users.noreply.github.com>
2025-02-09 16:51:09 +0100

    feat: Add Engagement component to IndexPage with new section and styling

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -5,14 +5,16 @@ import Contact from "../components/pages/contact/Contact";
 import Footer from "../components/pages/footer/Footer";
 import HowToWork from "../components/pages/howtowork/HowToWork";
 import HeaderV2 from "../components/pages/headerV2/HeaderV2";
-import AnimatedBanner from "../components/animated-banner/AnimatedBanner"; // Import du composant
+import AnimatedBanner from "../components/animated-banner/AnimatedBanner";
 import About from "../components/pages/about/About";
+import Engagement from "../components/pages/engagement/Engagement";
 
 function IndexPage(){
     return (
         <>
             <HeaderV2 />
             <Home />
+            <Engagement />
             <AnimatedBanner color="black" angle={-5} />
             <AnimatedBanner color="white" angle={5} />
             <About />
