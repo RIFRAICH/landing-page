78aa93fec925379049e7dd1bbc839b4b36b98c68
Yassinedevs <122100038+Yassinedevs@users.noreply.github.com>
2025-02-09 16:51:07 +0100

    feat: Add AnimatedBanner and About components to IndexPage

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -5,12 +5,17 @@ import Contact from "../components/pages/contact/Contact";
 import Footer from "../components/pages/footer/Footer";
 import HowToWork from "../components/pages/howtowork/HowToWork";
 import HeaderV2 from "../components/pages/headerV2/HeaderV2";
+import AnimatedBanner from "../components/animated-banner/AnimatedBanner"; // Import du composant
+import About from "../components/pages/about/About";
 
 function IndexPage(){
     return (
         <>
             <HeaderV2 />
             <Home />
+            <AnimatedBanner color="black" angle={-5} />
+            <AnimatedBanner color="white" angle={5} />
+            <About />
             <Concept />
             <HowToWork />
             <Why />
