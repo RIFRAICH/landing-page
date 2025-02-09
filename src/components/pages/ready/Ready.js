3d6c69114462059af799cff39e7c34db9e7e140a
Yassinedevs <122100038+Yassinedevs@users.noreply.github.com>
2025-02-09 17:57:53 +0100

    style: Align buttons horizontally with text in Ready component

--- src/components/pages/ready/Ready.js
+++ src/components/pages/ready/Ready.js
@@ -6,10 +6,12 @@ function Ready() {
         <section className="ready">
             <div className="ready__content">
                 <h2>Prêt à révolutionner votre manière<br />de vendre des vêtements<br />d'occasion ?</h2>
-                <p>Rejoignez une communauté de fripiers passionnés et dynamiques.</p>
-                <div className="ready__buttons">
-                    <button className="ready__button ready__button--dark">Lancez-vous</button>
-                    <button className="ready__button ready__button--light">En savoir plus</button>
+                <div className="ready__flex-container">
+                    <p>Rejoignez une communauté de fripiers passionnés et dynamiques.</p>
+                    <div className="ready__buttons">
+                        <button className="ready__button ready__button--dark">Lancez-vous</button>
+                        <button className="ready__button ready__button--light">En savoir plus</button>
+                    </div>
                 </div>
             </div>
         </section>
