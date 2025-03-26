2b520314d4415fac0b6f55fbcc60e2b6837d5fce
Clément Trempé <erpriex@gmail.com>
2025-03-26 22:30:23 +0100

    🐛 Fix UTF-8 in mail

--- public/mail.php
+++ public/mail.php
@@ -1,5 +1,5 @@
 <?php
-if(!isset($_POST['lastname']) ||!isset($_POST['firstname']) || !isset($_POST['email']) || !isset($_POST['subject']) || !isset($_POST['message'])){
+if(!isset($_POST['lastname']) || !isset($_POST['firstname']) || !isset($_POST['email']) || !isset($_POST['subject']) || !isset($_POST['message'])){
     echo 'false';
     return;
 }
@@ -11,9 +11,15 @@ $subject = strip_tags($_POST['subject']);
 $phone = isset($_POST['phone']) ? strip_tags($_POST['phone']) : '-';
 $message = strip_tags($_POST['message']);
 
-$headers = 'From: RIFRAICH <noreply@rifraich.fr>' . "\r\n" .
-           'Reply-To: '. $email . "\r\n" .
-           'X-Mailer: PHP/' . phpversion();
+$headers  = "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/plain; charset=utf-8\r\n";
+$headers .= "From: RIFRAICH <internal@rifraich.fr>\r\n";
+$headers .= "Reply-To: " . $email . "\r\n";
+$headers .= "X-Mailer: PHP/" . phpversion();
 
-mail("contact@rifraich.fr", "Message via landing page : ". $subject, "Nom : ". $lastname ."\r\nPrénom : ". $firstname ."\r\nEmail : ". $email ."\r\nTelephone : ". $phone ."\r\nObjet : ". $subject ."\r\n \r\n". $message, $headers);
+$subject_encoded = mb_encode_mimeheader("Message via landing page : " . $subject, "UTF-8", "B");
+
+mail("contact@rifraich.fr", $subject_encoded, "Nom : " . $lastname . "\r\nPrénom : " . $firstname . "\r\nEmail : " . $email . "\r\nTelephone : " . $phone . "\r\nObjet : " . $subject . "\r\n\r\n" . $message, $headers);
+
 echo 'true';
+?>
